2ff26904933b4d8875e1cda30010ac32c9b12f82
Matt Wittmann <mcwitt@gmail.com>
2022-10-21 17:28:26 -0700

    Add Haskell template

---
+++ flake.nix
@@ -0,0 +1,12 @@
+{
+  description = "Collection of project templates";
+
+  outputs = _: {
+    templates = {
+      haskell = {
+        path = ./haskell;
+        description = "Simple Haskell project using cabal and hpack";
+      };
+    };
+  };
+}
